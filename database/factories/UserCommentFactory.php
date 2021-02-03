e74fa5d66721b8371a9155fc0ac09e0f2600cb4d
Rainstorm <legrand.adrien96@gmail.com>
2021-02-03 14:50:01 +0100

    models + migrations + factories + seeders added

---
+++ database/factories/UserCommentFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\UserComment;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class UserCommentFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = UserComment::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
