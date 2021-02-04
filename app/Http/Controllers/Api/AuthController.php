d2c1391f0f3047520a5f5985e9e81e71c86ae23e
Rainstorm <legrand.adrien96@gmail.com>
2021-02-04 18:16:02 +0100

    AuthController added

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:55',
+            'email' => 'required|required|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        $user = User::create($validatedData);
+
+        $accessToken = $user->createToken('authToken')->accessToken;
+
+        return response(['username' => $user, 'accessToken' => $accessToken]);
+    }
+
+    public function login(Request $request)
+    {
+        $loginData = $request->validate([
+            'email' => 'required|required',
+            'password' => 'required'
+        ]);
+
+        if (!auth()->attempt($loginData)) {
+            return response(['message'=>'Invalid credentials']);
+        }
+
+        $accessToken = auth()->user()->createToken('authToken')->accessToken;
+
+        return response(['user'=> auth()->user(), 'accessToken' => $accessToken]);
+    }
+}
