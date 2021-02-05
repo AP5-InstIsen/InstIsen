327009c7a3b00e71f4f175e8caab35371c3696ef
Rainstorm <legrand.adrien96@gmail.com>
2021-02-05 23:58:31 +0100

    first code for image upload
    - controller without log test
    - table updated with nullable column for tests
    - model updated to take the path as creation element
    - api route added

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Image;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Storage;
 
 class ImageController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        if ($request->hasFile('image')) {
+            //  Let's do everything here
+            if ($request->file('image')->isValid()) {
+                //
+                $validated = $request->validate([
+                    'name' => 'string|max:255',
+                    'image' => 'mimes:jpeg,png|max:15360',
+                ]);
+                $path = $request->image->store('images');
+                $image = Image::create([
+                    'path' => $path
+                ]);
+
+                return response(['image' => $image, 'message' => 'image was uploaded successfully']);
+
+                //return response(['image' => $request, 'message' => 'image was uploaded successfully']);
+            }
+        }
+        return response(['message' => 'wrong file']);
+    }
+
+    /*public function viewUploads()
+    {
+        $images = File::all();
+        return view('view_uploads')->with('images', $images);
+    }*/
 }
--- app/Models/Image.php
+++ app/Models/Image.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'path',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+    ];
 }
