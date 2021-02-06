7504c375447ffe2d757b3615386dddef0bd26b12
Rainstorm <legrand.adrien96@gmail.com>
2021-02-06 19:20:23 +0100

    seeders updated
    - admin user added
    - roles added
    - unused seeders files removed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
+use App\Models\User;
+
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +18,24 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.admin',
+            'password' => Hash::make('admin'),
+            'role' => 1
+        ]);
+
+        Role::create([
+            'name' => 'administrator'
+        ]);
+
+        Role::create([
+            'name' => 'moderator'
+        ]);
+
+        Role::create([
+            'name' => 'user'
+        ]);
+
     }
 }
