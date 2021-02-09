a99dd2d6287dfdc7a9d91a279921bd6dd6b2c9aa
Rainstorm <legrand.adrien96@gmail.com>
2021-02-09 16:48:32 +0100

    Controller updated to create the note and update the image note

--- app/Http/Controllers/UserNoteController.php
+++ app/Http/Controllers/UserNoteController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Image;
+use App\Models\UserNote;
+use Auth;
 use Illuminate\Http\Request;
 
 class UserNoteController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        if (Auth::guard('api')->check()) {
+            $user = Auth::guard('api')->user();
+            $request->validate([
+                    'idImage' => 'required|integer',
+                    'note' => 'required|numeric',
+                ]);
+            $userNote = UserNote::create([
+                        'idUser' => $user->id,
+                        'idImage' => $request->idImage,
+                        'note' => $request->note
+                    ]);
+
+            $image = Image::find($request->idImage);
+            $image->note = ($image->note == null) ? $request->note : ($image->note + $request->note)/2;
+            $image->save();
+
+            return response(['userNote' => $userNote,'message' => 'the note was updated successfully']);
+        } else {
+            return response(['message' => 'you are not logged in']);
+        }
+    }
 }
