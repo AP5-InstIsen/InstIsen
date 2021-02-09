5f1c9fd299c3eac05311a6b9f2939aeb829672f6
Rainstorm <legrand.adrien96@gmail.com>
2021-02-09 17:34:51 +0100

    UserCommentController updated

--- app/Http/Controllers/UserCommentController.php
+++ app/Http/Controllers/UserCommentController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserComment;
+use Auth;
 use Illuminate\Http\Request;
 
 class UserCommentController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $user = Auth::guard('api')->user();
+        $request->validate([
+                    'idImage' => 'required|integer',
+                    'comment' => 'required|string|max:255',
+                ]);
+
+        $userComment = UserComment::create([
+                        'idUser' => $user->id,
+                        'idImage' => $request->idImage,
+                        'comment' => $request->comment
+                    ]);
+
+
+        return response(['userComment' => $userComment,'message' => 'the comment was updated successfully']);
+    }
 }
