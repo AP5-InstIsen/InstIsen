999ec9fa87117750eca98650a7a55bb613a525e6
Rainstorm <legrand.adrien96@gmail.com>
2021-02-09 17:18:59 +0100

    BroadcastListController updated

--- app/Http/Controllers/BroadcastListController.php
+++ app/Http/Controllers/BroadcastListController.php
@@ -11,54 +11,42 @@ class BroadcastListController extends Controller
 {
     public function store(Request $request)
     {
-        if (Auth::guard('api')->check()) {
-            $currentUser = Auth::guard('api')->user();
-            $validated = $request->validate([
+        $currentUser = Auth::guard('api')->user();
+        $validated = $request->validate([
                     'broadcast' => 'string'
             ]);
 
-            $users = User::all()->except($currentUser->id);
-            $newBroadcastList = "";
-            foreach (explode(',', $request->broadcast) as $userToBroadcast) {
-                foreach ($users as $user) {
-                    if ($user->id == intval($userToBroadcast)) {
-                        if ($newBroadcastList == "") {
-                            $newBroadcastList .= $user->id;
-                        } else {
-                            $newBroadcastList .= ','.$user->id;
-                        }
+        $users = User::all()->except($currentUser->id);
+        $newBroadcastList = "";
+        foreach (explode(',', $request->broadcast) as $userToBroadcast) {
+            foreach ($users as $user) {
+                if ($user->id == intval($userToBroadcast)) {
+                    if ($newBroadcastList == "") {
+                        $newBroadcastList .= $user->id;
+                    } else {
+                        $newBroadcastList .= ','.$user->id;
                     }
                 }
             }
+        }
 
-            $broadcast_list = BroadcastList::create([
+        $broadcast_list = BroadcastList::create([
                         'idUser' => $currentUser->id,
                         'broadcast' => $newBroadcastList
             ]);
 
-            return response(['broadcast_list' => $broadcast_list, 'message' => 'the broadcast list has been succressfully created']);
-        } else {
-            return response(['message' => 'you are not logged in']);
-        }
+        return response(['broadcast_list' => $broadcast_list, 'message' => 'the broadcast list has been succressfully created']);
     }
 
     public function getBroadcastListByUserId(Request $request)
     {
-        if (Auth::guard('api')->check()) {
-            $broadcast_list = BroadcastList::where('idUser', $request->id_user)->get();
-            return response(['broadcast_list' => $broadcast_list]);
-        } else {
-            return response(['message' => 'you are not logged in']);
-        }
+        $broadcast_list = BroadcastList::where('idUser', $request->id_user)->get();
+        return response(['broadcast_list' => $broadcast_list]);
     }
 
     public function getBroadcastListById(Request $request)
     {
-        if (Auth::guard('api')->check()) {
-            $broadcast_list = BroadcastList::find($request->id);
-            return response(['broadcast_list' => $broadcast_list]);
-        } else {
-            return response(['message' => 'you are not logged in']);
-        }
+        $broadcast_list = BroadcastList::find($request->id);
+        return response(['broadcast_list' => $broadcast_list]);
     }
 }
