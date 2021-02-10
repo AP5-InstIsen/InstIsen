e69a782d5081d0a1595550ce2f4a71f2f297b1ea
Guillaume <guillaume.vanhelst@gmail.com>
2021-02-10 19:47:52 +0100

    Creation of paths
    - Add web paths

--- resources/js/components/Header.js
+++ resources/js/components/Header.js
@@ -1,32 +1,51 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import Profile from "./Profile";
+import Picture from "./Picture";
+import Favorites from "./Favorites";
+import Home from "./Home";
+import Logout from "./Logout";
 
-const Header = () => (
-    <header>
-        <nav>
-            <ul class="menu align-right">
-                <li>
-                    <a href="#">One</a>
-                </li>
-                <li>
-                    <a href="#">Two</a>
-                </li>
-                <li>
-                    <a href="#">Three</a>
-                </li>
-                <li>
-                    <a href="#">Four</a>
-                </li>
-            </ul>
-            <div class="small 12 columns">
-                <div class="button-group">
-                    <a class="secondary button">View</a>
-                    <a class="success button">Edit</a>
-                    <a class="warning button">Share</a>
-                    <a class="alert button">Delete</a>
-                </div>
-            </div>
-        </nav>
-    </header>
-);
-
-export default Header;
+export default function Header() {
+    console.log("Header");
+    return (
+        <header>
+            <nav>
+                <BrowserRouter>
+                    <div>
+                        <ul class="menu align-right">
+                            <li>
+                                <Link to="/profile">Profile</Link>
+                            </li>
+                            <li>
+                                <Link to="/logout">Logout</Link>
+                            </li>
+                        </ul>
+                        <div class="small 12 columns">
+                            <div class="button-group">
+                                <Link to="/home">
+                                    <i class="fas fa-home"></i>
+                                    <a class="button">Home</a>
+                                </Link>
+                                <Link to="/picture">
+                                    <a class="button">Picture</a>
+                                </Link>
+                                <Link to="/favorites">
+                                    <a class="button">Favorites</a>
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="main-route-place">
+                            <Route exact path="/home" component={Home} />
+                            <Route exact path="/logout" component={Logout}/>
+                            <Route exact path="/profile" component={Profile} />
+                            <Route exact path="/picture" component={Picture} />
+                            <Route exact path="/favorites" component={Favorites} />
+                            
+                        </div>
+                    </div>
+                </BrowserRouter>
+            </nav>
+        </header>
+    );
+}
