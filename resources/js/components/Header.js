50282c4d15b7d42a0f2e1288eabdeeb99e91849a
Guillaume <guillaume.vanhelst@gmail.com>
2021-02-28 18:07:21 +0100

    New Foundation Header

--- resources/js/components/Header.js
+++ resources/js/components/Header.js
@@ -7,41 +7,77 @@ import Home from "./Home";
 import Logout from "./Logout";
 
 export default function Header() {
-    console.log("Header");
     return (
         <header>
             <nav>
                 <BrowserRouter>
                     <div>
-                        <ul class="menu align-right">
-                            <li>
-                                <Link to="/profile">Profile</Link>
-                            </li>
-                            <li>
-                                <Link to="/logout">Logout</Link>
-                            </li>
-                        </ul>
-                        <div class="small 12 columns">
-                            <div class="button-group">
-                                <Link to="/home">
-                                    <i class="fas fa-home"></i>
-                                    <a class="button">Home</a>
-                                </Link>
-                                <Link to="/picture">
-                                    <a class="button">Picture</a>
-                                </Link>
-                                <Link to="/favorites">
-                                    <a class="button">Favorites</a>
-                                </Link>
+                        <div
+                            class="title-bar"
+                            data-responsive-toggle="example-animated-menu"
+                            data-hide-for="medium"
+                        >
+                            <button
+                                class="menu-icon"
+                                type="button"
+                                data-toggle
+                            ></button>
+                            <div class="title-bar-title">Menu</div>
+                        </div>
+                        <div
+                            class="top-bar"
+                            id="example-animated-menu"
+                            data-animate="hinge-in-from-top spin-out"
+                        >
+                            <div class="top-bar-left">
+                                <ul class="dropdown menu" data-dropdown-menu>
+                                    <li class="menu-text">InstISEN</li>
+                                    <li>
+                                        <Link to="/home">Home</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/picture">Picture</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/favorites">Favorites</Link>
+                                    </li>
+                                </ul>
                             </div>
+                            <div class="top-bar-middle">
+                                <ul class="dropdown menu" data-dropdown-menu>
+                                    <li>
+                                        <input
+                                            type="search"
+                                            placeholder="Search"
+                                        />
+                                    </li>
+                                    <li>
+                                        <button type="button" class="button">
+                                            Search
+                                        </button>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="top-bar-right"></div>
+                            <ul class="dropdown menu" data-dropdown-menu>
+                                <li>
+                                    <Link to="/profile">Profile</Link>
+                                </li>
+                                <li>
+                                    <Link to="/logout">Logout</Link>
+                                </li>
+                            </ul>
                         </div>
                         <div className="main-route-place">
                             <Route exact path="/home" component={Home} />
-                            <Route exact path="/logout" component={Logout}/>
+                            <Route exact path="/logout" component={Logout} />
                             <Route exact path="/profile" component={Profile} />
                             <Route exact path="/picture" component={Picture} />
-                            <Route exact path="/favorites" component={Favorites} />
-                            
+                            <Route
+                                exact
+                                path="/favorites"
+                                component={Favorites}
+                            />
                         </div>
                     </div>
                 </BrowserRouter>
