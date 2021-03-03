53c628375b7f029d932fadf741e6028be1f0c699
Guillaume <guillaume.vanhelst@gmail.com>
2021-03-03 18:10:19 +0100

    Create Login Form foundation

--- resources/js/components/LoginForm.js
+++ resources/js/components/LoginForm.js
@@ -1,66 +1,115 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
+import "../../css/styles.css";
+import "./LoginAnimators";
 
-
-async function LoginUser(userInfo){
-    return await axios.post('/api/login', userInfo)
-        .then(res =>{
-            return res.data.accessToken;
-        })
+async function LoginUser(userInfo) {
+    return await axios.post("/api/login", userInfo).then((res) => {
+        return res.data.accessToken;
+    });
 }
 
-
-
-
-export default function LoginForm({setToken, setIsregister})
-{
+export default function LoginForm({ setToken, setIsregister }) {
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
 
-    const handleSubmit = async e => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        const token =  LoginUser({
+        const token = LoginUser({
             email,
             password,
         });
-        token.then(r => {
-            setToken(r)
-        })
-    }
-    const handleRegister= async e => {
-        console.log("click")
+        token.then((r) => {
+            setToken(r);
+        });
+    };
+    const handleRegister = async (e) => {
+        console.log("click");
         setIsregister("1");
-    }
+    };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div className="grid-container">
-                <div className="grid-x grid-padding-x">
-                    <div className="medium-6 cell">
-                        <label>Email
-                            <input
-                                type="text"
-                                name="email"
-                                placeholder="john@doe.com"
-                                onChange={e => setEmail(e.target.value)}
-                            />
-                        </label>
-                        <label>Password
-                            <input
-                                type="password"
-                                name="password"
-                                onChange={e => setPassword(e.target.value)}
-                            />
-                        </label>
-                        <input type="submit" value="Connexion"/>
-                        <p className="help-text" >pas encore inscrit? clique <a onClick={handleRegister} >ici</a></p>
-                    </div>
+        <section className="forms-section">
+            <h1 className="section-title">InstISEN</h1>
+            <div className="forms">
+                <div className="form-wrapper is-active">
+                    <button type="button" className="switcher switcher-login">
+                        Login
+                        <span className="underline"></span>
+                    </button>
+                    <form className="form form-login">
+                        <fieldset>
+                            <legend>
+                                Please, enter your email and password for login.
+                            </legend>
+                            <div className="input-block">
+                                <label htmlFor="login-email">E-mail</label>
+                                <input
+                                    id="login-email"
+                                    type="email"
+                                    name="email"
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <div className="input-block">
+                                <label htmlFor="login-password">Password</label>
+                                <input
+                                    id="login-password"
+                                    type="password"
+                                    name="password"
+                                    onChange={(e) =>
+                                        setPassword(e.target.value)
+                                    }
+                                    required
+                                />
+                            </div>
+                        </fieldset>
+                        <button
+                            type="submit"
+                            className="btn-login"
+                            onSubmit={handleSubmit}
+                        >
+                            Login
+                        </button>
+                    </form>
+                </div>
+                <div className="form-wrapper">
+                    <button type="button" className="switcher switcher-signup">
+                        Sign Up
+                        <span className="underline"></span>
+                    </button>
+                    <form className="form form-signup">
+                        <fieldset>
+                            <legend>
+                                Please, enter your email, password and password
+                                confirmation for sign up.
+                            </legend>
+                            <div className="input-block">
+                                <label htmlFor="signup-email">E-mail</label>
+                                <input
+                                    id="signup-email"
+                                    type="email"
+                                    required
+                                />
+                            </div>
+                            <div className="input-block">
+                                <label htmlFor="signup-password">
+                                    Password
+                                </label>
+                                <input
+                                    id="signup-password"
+                                    type="password"
+                                    required
+                                />
+                            </div>
+                        </fieldset>
+                        <button type="submit" className="btn-signup">
+                            Continue
+                        </button>
+                    </form>
                 </div>
             </div>
-        </form>
+        </section>
     );
 }
-
-
-
-
