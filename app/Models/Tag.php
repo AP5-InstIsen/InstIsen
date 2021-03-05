2691466cb8c8d13c5f3fdd0edb051b94ffa2f939
Rainstorm <legrand.adrien96@gmail.com>
2021-03-05 16:44:55 +0100

    model updated to use the columns idImage and name

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'idImage',
+        'name'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+    ];
 }
